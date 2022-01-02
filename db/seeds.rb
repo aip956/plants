75da23eb04bf710935ac68ca2eb32759469481eb
Anthea Ip <antheaip964@gmail.com>
2022-01-01 20:33:30 -0500

    Saving

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,62 +13,71 @@ Plant.create([
     "lastWatered": "2021/12/25",
     "lastFed": "2021/12/01",
     "recWater": "1/week, mist 3x/week",
-    "recFeed": "1/mo, Spring & Summer"
+    "recFeed": "1/mo, Spring & Summer",
+    "user_id": "1"
     },
     {"plantname": "Snake",
     "planttype": "Calathea Rattlesnake",
     "lastWatered": "2021/12/25",
     "lastFed": "2021/12/01",
     "recWater": "1/week, mist 3x/week",
-    "recFeed": "1/mo, Spring & Summer"
+    "recFeed": "1/mo, Spring & Summer",
+    "user_id": "1"
     },
     {"plantname": "Biggie",
     "planttype": "Bamboo Palm",
     "lastWatered": "2021/12/25",
     "lastFed": "2021/12/01",
     "recWater": "1/week, mist 3x/week",
-    "recFeed": "Half/mo, Spring & Summer"
+    "recFeed": "Half/mo, Spring & Summer",
+    "user_id": "1"
     },
     {"plantname": "Pink",
     "planttype": "Bromeliad Aechmea",
     "lastWatered": "2021/12/25",
     "lastFed": "2021/12/01",
     "recWater": "1/week, mist 3x/week",
-    "recFeed": "Foliar spray 1-2/mo spring & summer"
+    "recFeed": "Foliar spray 1-2/mo spring & summer",
+    "user_id": "1"
     },
     {"plantname": "Green Prayer",
     "planttype": "Neon Prayer Plant",
     "lastWatered": "2021/12/25",
     "lastFed": "2021/12/01",
     "recWater": "1/week, mist 3x/week",
-    "recFeed": "1/mo, Spring & Summer"
+    "recFeed": "1/mo, Spring & Summer",
+    "user_id": "1"
     },
     {"plantname": "Shedders",
     "planttype": "Kimberly Queen Fern",
     "lastWatered": "2021/12/25",
     "lastFed": "2021/12/01",
     "recWater": "1/week, mist 3x/week",
-    "recFeed": "Half/3mo"
+    "recFeed": "Half/3mo",
+    "user_id": "1"
     },
     {"plantname": "Big Croc",
     "planttype": "Crocodile Fern",
     "lastWatered": "2021/12/25",
     "lastFed": "2021/12/01",
     "recWater": "1/week, mist 3x/week",
-    "recFeed": "Half/mo, Spring & Summer"
+    "recFeed": "Half/mo, Spring & Summer",
+    "user_id": "1"
     },
     {"plantname": "Small Bird",
     "planttype": "Bird’s Nest Fern",
     "lastWatered": "2021/12/25",
     "lastFed": "2021/12/01",
     "recWater": "1/week, mist 3x/week",
-    "recFeed": "1/mo, Spring & Summer"
+    "recFeed": "1/mo, Spring & Summer",
+    "user_id": "1"
     },
     {"plantname": "Dying Succ",
     "planttype": "Echeveria Rosea",
     "lastWatered": "2021/12/14",
     "lastFed": "2021/12/01",
     "recWater": "1/2weeks",
-    "recFeed": "1/season"
+    "recFeed": "1/season",
+    "user_id": "1"
     },
 ])
